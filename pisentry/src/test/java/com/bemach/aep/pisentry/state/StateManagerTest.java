20cfe91aa1b9cea727777553410eace9c8445e96
drtran <ktran@bemach.com>
2016-02-02 16:35:29 -0500

    Refactored to simplify test code

--- pisentry/src/test/java/com/bemach/aep/pisentry/state/StateManagerTest.java
+++ pisentry/src/test/java/com/bemach/aep/pisentry/state/StateManagerTest.java
@@ -10,35 +10,38 @@ import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.bemach.aep.pisentry.vos.Event;
 import com.bemach.aep.pisentry.vos.EventType;
 import com.bemach.aep.pisentry.vos.Notification;
 import com.bemach.aep.pisentry.vos.State;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StateManagerTest {
 	
 	private StateManager target;
 	
-	@Mock
-	private Event arm;
+	@Mock private Event arm;
 
-	private NotificationManager notifier;
+	@Mock private NotificationManager notifier;
 
-	private Notification msg;
+	@Mock private Notification msg;
+
+	@Mock private Event fault;
+
+	@Mock private Event disarm;
 
 	@Before
 	public void setUp() {
-		notifier = mock(NotificationManager.class);
 		msg = mock(Notification.class);
-//		Mockito.doNothing().when(notifier).notify(Mockito.any(Notification.class)); by default mock is to do nothing.
 		target = new StateManagerImpl(notifier);
 	}
 	
 	@Test
 	public void should_stay_unarmed_when_unarmed_and_get_fault() {
-		Event fault = mock(Event.class);
 		when(fault.getType()).thenReturn(EventType.FAULT);
 		
 		target.process(fault);
@@ -49,8 +52,6 @@ public class StateManagerTest {
 	
 	@Test
 	public void should_become_alarmed_when_armed_away_and_get_fault_event() {
-		Event fault = mock(Event.class);
-		Event arm = mock(Event.class);
 		when(arm.getType()).thenReturn(EventType.ARM_AWAY);
 		when(fault.getType()).thenReturn(EventType.FAULT);
 		target.process(arm);
@@ -63,8 +64,6 @@ public class StateManagerTest {
 	
 	@Test 
 	public void should_become_unarmed_when_armed_away_and_get_disarm_event() {
-		Event disarm = mock(Event.class);
-		Event arm = mock(Event.class);
 		when(arm.getType()).thenReturn(EventType.ARM_AWAY);
 		when(disarm.getType()).thenReturn(EventType.DISARM);
 		target.process(arm);
@@ -77,8 +76,6 @@ public class StateManagerTest {
 	
 	@Test
 	public void should_notify_when_state_changes() {
-		Event disarm = mock(Event.class);
-		Event arm = mock(Event.class);
 		when(arm.getType()).thenReturn(EventType.ARM_AWAY);
 		when(disarm.getType()).thenReturn(EventType.DISARM);
 
