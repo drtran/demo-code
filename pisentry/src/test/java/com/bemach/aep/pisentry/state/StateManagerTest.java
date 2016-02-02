85b8ab50882b124aaeafc2003b87dd0811d9ba80
drtran <ktran@bemach.com>
2016-02-02 13:28:05 -0500

    rename test methods for readability - test the behaviors, not methods

--- pisentry/src/test/java/com/bemach/aep/pisentry/state/StateManagerTest.java
+++ pisentry/src/test/java/com/bemach/aep/pisentry/state/StateManagerTest.java
@@ -25,25 +25,32 @@ public class StateManagerTest {
 	}
 	
 	@Test
-	public void shouldNotGenerateAlarmWhenUnArmedAndReceivedFaultEvent() {
+	public void should_stay_unarmed_when_unarmed_and_get_fault() {
 		Event fault = Mockito.mock(Event.class);
 		Mockito.when(fault.getType()).thenReturn(EventType.FAULT);
+		
 		target.process(fault);
+		
 		State state = target.getState(); 
 		assertNotEquals(State.ALARMED, state);
 	}
 	
 	@Test
-	public void shouldGenerateAlarmWhenArmedAndReceivedFaultEvent() {
+	public void should_become_alarmed_when_armed_and_get_fault_event() {
 		Event fault = Mockito.mock(Event.class);
 		Event arm = Mockito.mock(Event.class);
 		Mockito.when(arm.getType()).thenReturn(EventType.ARM_AWAY);
 		Mockito.when(fault.getType()).thenReturn(EventType.FAULT);
 		target.process(arm);
+		
 		target.process(fault);
+		
 		State state = target.getState(); 
 		assertEquals(State.ALARMED, state);
 	}
 	
-	
+	@Test 
+	public void should_become_unarmed_when_armed_and_get_disarm_event() {
+		
+	}
 }
