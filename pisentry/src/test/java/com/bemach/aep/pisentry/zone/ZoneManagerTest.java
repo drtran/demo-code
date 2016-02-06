70a29f08ba7cb2a6c0edd38fcb976250323204e2
drtran <ktran@bemach.com>
2016-02-06 18:07:26 -0500

    Added @Parameters JUnitParams to test multiple cases

--- pisentry/src/test/java/com/bemach/aep/pisentry/zone/ZoneManagerTest.java
+++ pisentry/src/test/java/com/bemach/aep/pisentry/zone/ZoneManagerTest.java
@@ -7,10 +7,14 @@ import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.bemach.aep.pisentry.vos.Zone;
-import com.bemach.aep.pisentry.vos.ZoneType;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class ZoneManagerTest {
 	@Test
 	public void shouldBeEmptyWhenStarted() {
@@ -28,14 +32,19 @@ public class ZoneManagerTest {
 	}
 
 	@Test
-	public void shouldLoadOneItemZoneFromInputStream() throws IOException {
+	@Parameters(method = "inputValues")
+	public void shouldLoadOneItemZoneFromInputStream(String input) throws IOException {
 		ZoneManager target = new ZoneManagerImpl();
-		ByteArrayInputStream bais = new ByteArrayInputStream(
-				"57,Garrage-Left,REED,Left Garage Door,06-FEB-2016".getBytes());
+		ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
 		target.load(bais);
 		bais.close();
 		List<Zone> zoneList = target.getZoneList();
 		assertEquals(1, zoneList.size());
-		assertEquals("57,Garrage-Left,REED,Left Garage Door,06-FEB-2016", zoneList.get(0).toString());
+		assertEquals(input, zoneList.get(0).toString());
+	}
+
+	private Object[] inputValues() {
+		return new Object[] { new Object[] { "57,Garrage-Left,REED,Left Garage Door,06-FEB-2016" },
+				new Object[] { "58,Garrage-Right,REED,Left Garage Door,06-FEB-2016" } };
 	}
 }
