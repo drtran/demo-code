791b91e093f4da5749b6546919bcbd8b5171910e
drtran <ktran@bemach.com>
2016-02-14 07:47:42 -0500

    Refactored to remove duplication in Dollar class

--- tdd/src/main/java/com/bemach/aep/tdd/Dollar.java
+++ tdd/src/main/java/com/bemach/aep/tdd/Dollar.java
@@ -2,15 +2,14 @@ package com.bemach.aep.tdd;
 
 public class Dollar {
 
-	public int amount = 10;
+	public int amount;
 
 	public Dollar(int amount) {
-		// TODO Auto-generated constructor stub
+		this.amount = amount;
 	}
 
 	public void times(int multiplier) {
-		// TODO Auto-generated method stub
-		
+		amount *= multiplier;
 	}
 
 }
