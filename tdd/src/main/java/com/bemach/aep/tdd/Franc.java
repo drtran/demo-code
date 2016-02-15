9142651c1a96f5ac6e3c67a259a0c5599a68c268
drtran <ktran@bemach.com>
2016-02-15 07:48:55 -0500

    Refactored to have Franc extends Money.

--- tdd/src/main/java/com/bemach/aep/tdd/Franc.java
+++ tdd/src/main/java/com/bemach/aep/tdd/Franc.java
@@ -1,20 +1,12 @@
 package com.bemach.aep.tdd;
 
-public class Franc {
-
-	private int amount;
+public class Franc extends Money {
 
 	public Franc(int amount) {
 		this.amount = amount;
 	}
 
-	public Franc times(int multiplier) {
+	public Money times(int multiplier) {
 		return new Franc(amount * multiplier);
 	}
-
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
-	}
-
-}
+} 
