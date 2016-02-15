df9c4cc0d50a2d691c8e406adbd31c3862037f18
drtran <ktran@bemach.com>
2016-02-15 06:28:11 -0500

    Added new testFrancMultiplication test

--- tdd/src/test/java/com/bemach/aep/tdd/MoneyTest.java
+++ tdd/src/test/java/com/bemach/aep/tdd/MoneyTest.java
@@ -11,10 +11,18 @@ public class MoneyTest {
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
+	
+	@Test
+	public void tesFranctMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
 
 	@Test
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertTrue(new Dollar(6).equals(new Dollar(6)));
 	}
+	
 }
