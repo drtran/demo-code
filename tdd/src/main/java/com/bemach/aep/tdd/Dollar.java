0c0441b3a6889499b107a235c7016c0ab4bf6d90
drtran <ktran@bemach.com>
2016-02-15 07:41:06 -0500

    Refactored by pulling up equals() method to supperclass

--- tdd/src/main/java/com/bemach/aep/tdd/Dollar.java
+++ tdd/src/main/java/com/bemach/aep/tdd/Dollar.java
@@ -9,9 +9,4 @@ public class Dollar extends Money {
 	public Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
-	}
 }
