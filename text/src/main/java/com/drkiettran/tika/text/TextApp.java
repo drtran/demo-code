888b1d811e6326486120fe3bd348476cd1777d48
drtran <ktran@bemach.com>
2017-05-26 06:16:11 -0400

    Pass first unit test

--- text/src/main/java/com/drkiettran/tika/text/TextApp.java
+++ text/src/main/java/com/drkiettran/tika/text/TextApp.java
@@ -1,11 +1,16 @@
 package com.drkiettran.tika.text;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
 
 public class TextApp {
 
-	public String parseToString(InputStream is) {
-		return "";
+	public String parseToString(InputStream is) throws IOException, TikaException {
+		Tika tika = new Tika();
+		return tika.parseToString(is);
 	}
 
 }
--- text/src/test/java/com/drkiettran/tika/text/TextAppTest.java
+++ text/src/test/java/com/drkiettran/tika/text/TextAppTest.java
@@ -1,23 +1,27 @@
 package com.drkiettran.tika.text;
 
-import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.tika.exception.TikaException;
 import org.junit.Test;
 
 public class TextAppTest {
 	@Test
-	public void given_a_pdf_content_i_should_get_a_main_text() throws IOException {
+	public void given_a_pdf_content_i_should_get_a_main_text() throws IOException, TikaException {
 		TextApp textApp = new TextApp();
 		String text = null;
 
-		try (InputStream is = this.getClass().getResourceAsStream("tika.pdf")) {
+		try (InputStream is = TextApp.class.getResourceAsStream("/Tika-examples.pdf")) {
 			text = textApp.parseToString(is);
 		}
+
 		assertThat(text, not(isEmptyOrNullString()));
+		assertThat(text, containsString("This page provides"));
 	}
 }
