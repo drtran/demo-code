0cac9db35c633a36095f1d60f55d0eba65bafc58
drtran <ktran@bemach.com>
2017-05-26 06:27:01 -0400

    Added docx file and refactored code for duplication

--- text/src/test/java/com/drkiettran/tika/text/TextAppTest.java
+++ text/src/test/java/com/drkiettran/tika/text/TextAppTest.java
@@ -9,19 +9,42 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.tika.exception.TikaException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextAppTest {
+
+	private static final String TIKA_EXAMPLES_DOCX = "/Tika-examples.docx";
+	private static final String EXPECTED_STRING = "This page provides";
+	private static final String TIKA_EXAMPLES_PDF = "/Tika-examples.pdf";
+	private TextApp textApp = null;
+
+	@Before
+	public void setUp() {
+		textApp = new TextApp();
+	}
+
 	@Test
 	public void given_a_pdf_content_i_should_get_a_main_text() throws IOException, TikaException {
-		TextApp textApp = new TextApp();
 		String text = null;
 
-		try (InputStream is = TextApp.class.getResourceAsStream("/Tika-examples.pdf")) {
+		try (InputStream is = TextApp.class.getResourceAsStream(TIKA_EXAMPLES_PDF)) {
 			text = textApp.parseToString(is);
 		}
 
 		assertThat(text, not(isEmptyOrNullString()));
-		assertThat(text, containsString("This page provides"));
+		assertThat(text, containsString(EXPECTED_STRING));
+	}
+
+	@Test
+	public void given_a_worddocx_content_i_should_get_a_main_text() throws IOException, TikaException {
+		String text = null;
+
+		try (InputStream is = TextApp.class.getResourceAsStream(TIKA_EXAMPLES_DOCX)) {
+			text = textApp.parseToString(is);
+		}
+
+		assertThat(text, not(isEmptyOrNullString()));
+		assertThat(text, containsString(EXPECTED_STRING));
 	}
 }
