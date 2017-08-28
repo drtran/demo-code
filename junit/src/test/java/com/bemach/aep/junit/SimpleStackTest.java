bc998741aa81f6169fd2ad449be58e4a100ef35e
drtran <ktran@bemach.com>
2017-08-28 07:38:54 -0400

    Updated parameterized object initialization to use String instead

--- junit/src/test/java/com/bemach/aep/junit/SimpleStackTest.java
+++ junit/src/test/java/com/bemach/aep/junit/SimpleStackTest.java
@@ -5,20 +5,21 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith; 
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(value=Parameterized.class)
+@RunWith(value = Parameterized.class)
 public class SimpleStackTest {
- 
+
 	private static final String HELLO_AMERICA = "Hello, America!";
 	private static final String HELLO_WORLD = "Hello, world!";
 	private SimpleStack<Object> target = null;
-	private Object[] objectList;
+	private List<String> stringList;
 
 	@Test
 	public void shouldBeEmptyWhenCreated() {
@@ -32,50 +33,50 @@ public class SimpleStackTest {
 		target = new SimpleStack<Object>();
 	}
 
-	@Test (expected=ArrayIndexOutOfBoundsException.class)
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void shouldThrowExceptionWhenPopOnEmpty() {
 		// Act
 		target.pop();
 	}
-	
+
 	@Parameters
-	public static Collection<Object[][]> getTestParameters() {
-	  return Arrays.asList(new Object[][][] { 
-		    {{HELLO_WORLD}}, 
-		    {{HELLO_WORLD, HELLO_AMERICA}}  
-		  }); 
-	  }
-	
-	  public SimpleStackTest(Object[] objectList) {
-	    this.objectList = objectList;
-	  }
-	   
-	  @Test
-	  public void shouldPushItemsOntoStack() {
-		  // Act
-		  for (Object object: objectList) {
-			  target.push(object);
-		  }
-		  
-		  // Assert
-		  assertThat(target.size(), equalTo(objectList.length));
-	  }
-	  
-	  @Test
-	  public void shouldPopItemsOffTheStack() {
-		  // Arrange
-		  for (Object object: objectList) {
-			  target.push(object);
-		  } 
-		  
-		  // Act
-		  Object actual = null;
-		  for (int i = 0; i < objectList.length; i++) {
-			  actual = target.pop();
-		  }
-		  
-		  // Assert
-		  assertThat(actual, equalTo(objectList[0]));
-	  }
+	public static Collection<String[][]> getTestParameters() {
+		return Arrays.asList(new String[][][] { 
+			{ { HELLO_WORLD } }, 
+			  { { HELLO_WORLD, HELLO_AMERICA } } 
+			});
+	}
+
+	public SimpleStackTest(Object[] objectList) {
+		this.stringList = Arrays.asList((String[]) objectList);
+	}
+
+	@Test
+	public void shouldPushItemsOntoStack() {
+		// Act
+		for (Object object : stringList) {
+			target.push(object);
+		}
+
+		// Assert
+		assertThat(target.size(), equalTo(stringList.size()));
+	}
+
+	@Test
+	public void shouldPopItemsOffTheStack() {
+		// Arrange
+		for (Object object : stringList) {
+			target.push(object);
+		}
+
+		// Act
+		Object actual = null;
+		for (int i = 0; i < stringList.size(); i++) {
+			actual = target.pop();
+		}
+
+		// Assert
+		assertThat(actual, equalTo(stringList.get(0)));
+	}
 
 }
