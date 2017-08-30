155484f16381522b81b62c23d0264b38db488503
drtran <ktran@bemach.com>
2017-08-30 05:17:20 -0400

    Added standard in as stream example

--- fp/src/main/java/com/drkiettran/java8/fp/App.java
+++ fp/src/main/java/com/drkiettran/java8/fp/App.java
@@ -1,11 +1,27 @@
 package com.drkiettran.java8.fp;
 
+import java.util.Scanner;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 /**
- * Hello world!
+ * Sample Code from Jessica Kerr
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		readFromStdin("Knock knock. ").anyMatch(s -> s.equals("Who's there?"));
+		System.out.println("Boo.");
+		readFromStdin("").anyMatch(s -> s.equals("Boo who?"));
+		System.out.println("You don't have to cry about it.");
+	}
+
+	private static Stream<String> readFromStdin(String prompt) {
+		Scanner scanner = new Scanner(System.in);
+		Supplier<String> stdin = () -> {
+			System.out.print(prompt);
+			return scanner.nextLine();
+		};
+		return Stream.generate(stdin);
 	}
 }
